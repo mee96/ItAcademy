fff110ab0abca917cf26ddabd480cca406ee0d29
Sasha <safontsanek@gmail.com>
2026-01-13 11:38:26 +0100

    first commit

--- Ejercisis/13-1/src/Main.java
+++ Ejercisis/13-1/src/Main.java
@@ -3,5 +3,5 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("Hello World!");
+    System.out.println("Hello Worldddd!");
 }
