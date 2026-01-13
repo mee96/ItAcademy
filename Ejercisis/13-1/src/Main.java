baba855658c1978d56cd7c35c85ba9e679a0d9fe
Sasha <safontsanek@gmail.com>
2026-01-13 11:41:10 +0100

    first commit

--- Ejercisis/13-1/src/Main.java
+++ Ejercisis/13-1/src/Main.java
@@ -4,4 +4,5 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.println("Hello Worldddd!");
+    // primer dia de java
 }
