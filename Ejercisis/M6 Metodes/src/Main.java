f8d557cf134de6a2407f70700ed0be2f40f32765
Sasha <safontsanek@gmail.com>
2026-01-14 12:12:56 +0100

    M6 acabat

---
+++ Ejercisis/M6 Metodes/src/Main.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+
+    public static void main(String[] args) {
+        myMethod("Carme", 29);
+        myMethod("Victoria", 20);
+        myMethod("Rodney", 26);
+    }
+}
+
+
+/*
+
+En aquest mòdul aprendràs a crear mètodes a la classe principal.
+Aquests mètodes seran els que comunament s'anomenen funcions en altres
+llenguatges de programació.
+
+» Entendre perquè utilitzem les funcions i els mètodes.
+» Aprendre la diferència entre funcions i mètodes.
+» Aprendre com funciona el pas de paràmetres.
+» Aprendre com podem retornar dades.
+» Entendre el concepte i les diferències entre una variable local i una variable global.
+
+
+
+ */
